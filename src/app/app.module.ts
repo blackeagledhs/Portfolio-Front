465d249d1d0bc75d4ba92b5a9d350f908a3c084f
Black Eagle <blackeagledhs@gmail.com>
2022-03-30 08:56:30 -0300

    Primeros Cambios
    Creación de componentes y servicios, se agrego bootstrap, bootstrap-icons y fontawesome.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { StudiesComponent } from './components/studies/studies.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    AboutComponent,
+    ExperiencesComponent,
+    StudiesComponent,
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
